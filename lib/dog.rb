5a080ee51126bec7493822d5248c4b43a18a3ac3
danny galimi <dannygalimi13@aim.com>
2021-02-24 01:52:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,7 @@ class Dog
   end 
   
   def self.create(hash)
-    dog = Dog.new({id: hash[0], name: hash[1], breed: hash[2]})
+    dog = Dog.new({id: hash[0][0], name: hash[0][1], breed: hash[0][2]})
     dog.save
     dog
   end
