003a8197253e0be95922a3781cab40cd93c144ef
danny galimi <dannygalimi13@aim.com>
2021-02-24 01:18:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,8 +39,8 @@ class Dog
   end 
   end 
   
-  def self.create(id:,name:,breed:)
-    dog = Dog.new(id:,name:,breed:)
+  def self.create(name:,breed:)
+    dog = Dog.new(name:,breed:)
     dog.save
     dog
   end
