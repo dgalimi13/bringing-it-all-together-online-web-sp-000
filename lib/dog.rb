1376591abcd11915e4ec6ee8f25388c3a88de905
danny galimi <dannygalimi13@aim.com>
2021-02-24 03:05:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,10 +74,10 @@ class Dog
     dog
   end 
   
-  def self.find_by_name
-    sql = "SELECT * FROM songs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
-    Song.new(result[0], result[1], result[2])
+  def self.find_by_name(name:)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
   end 
   
   def update 
